4b1d0046f39b9314e03b4fc1020d0d8eb8e118fb
Ramon Vloon <Ramon.Vloon@effectory.com>
2018-09-14 10:51:17 +0200

    added new images

--- src/create-session.ts
+++ src/create-session.ts
@@ -1,21 +1,49 @@
 import { Session } from './models/session';
 
 export class CreateSession {
-    public session: Session;
-    public newTeamMemberName : string;
-
-    constructor(){
-
+  public session: Session;
+  public newTeamMemberName: string;
+  public images = [
+    {
+      path: "src/images/Blond.svg"
+    },
+    {
+      path: "src/images/Bruin.svg"
+    },
+    {
+      path: "src/images/Hans.svg"
+    },
+    {
+      path: "src/images/Johannes.svg"
+    },
+    {
+      path: "src/images/Kaal.svg"
+    },
+    {
+      path: "src/images/KnotBlond.svg"
+    },
+    {
+      path: "src/images/KnotBruin.svg"
+    },
+    {
+      path: "src/images/Ramon.svg"
+    },
+    {
+      path: "src/images/Roy.svg"
+    },
+    {
+      path: "src/images/Sander.svg"
     }
+  ];
 
-    public created(){
-        this.session = new Session();
-        console.log(this.session);
-    }
+  public created() {
+    this.session = new Session();
+    console.log(this.session);
+  }
 
-    public addTeamMember(name: string){
-        this.session.addTeamMember(name);
-        this.newTeamMemberName = '';
-    }
+  public addTeamMember(name: string) {
+    this.session.addTeamMember(name);
+    this.newTeamMemberName = '';
+  }
 
 } 
