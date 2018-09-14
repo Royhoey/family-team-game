4c8fa0b15d5473cccd2379ded952d351c16f8f4b
Ramon Vloon <Ramon.Vloon@effectory.com>
2018-09-14 09:52:30 +0200

    added persons and updates

--- src/app.ts
+++ src/app.ts
@@ -1,78 +1,15 @@
+
+
+import { autoinject } from 'aurelia-framework';
+import { AppRouter, RouterConfiguration } from 'aurelia-router';
+@autoinject
 export class App {
-  public chars = [
-    {
-      name: "Sander",
-      image: "char1.svg"
-    },
-    {
-      name: "Roy",
-      image: "char2.svg"
-    }
-  ];
-  constructor() {
-    // this is used later in the resizing and gesture demos
-    (<any>window).dragMoveListener = this.dragMoveListener;
-    interact('.draggable')
-      .draggable({
-        // enable inertial throwing
-        inertia: true,
-        // keep the element within the area of it's parent
-        restrict: {
-          restriction: "parent",
-          endOnly: true,
-          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-        },
-        // enable autoScroll
-        autoScroll: true,
 
-        // call this function on every dragmove event
-        onmove: this.dragMoveListener,
-        // call this function on every dragend event
-        onend: (event) => {
-          var textEl = event.target.querySelector('p');
 
-          textEl && (textEl.textContent =
-            'moved a distance of '
-            + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-              Math.pow(event.pageY - event.y0, 2) | 0))
-              .toFixed(2) + 'px');
-        }
-      });
-
-  }
-  public rotate(direction, id) {
-    var innerArrow = document.getElementById(id);
-
-    switch (direction) {
-      case "left":
-        innerArrow.setAttribute("transform", "rotate(180)");
-        break;
-      case "up":
-        innerArrow.setAttribute("transform", "rotate(270)");
-        break;
-      case "right":
-        innerArrow.setAttribute("transform", "rotate(0)");
-        break;
-      case "down":
-        innerArrow.setAttribute("transform", "rotate(90)");
-        break;
-    }
+  private configureRouter(config: RouterConfiguration, router: AppRouter) {
+    config.map([
+      { route: [''], name: 'home', moduleId: './team-positions' },
+    ]);
   }
 
-  public dragMoveListener(event) {
-    console.log(event.target);
-    var target = event.target,
-      // keep the dragged position in the data-x/data-y attributes
-      x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-      y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-    // translate the element
-    target.style.webkitTransform =
-      target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
-
-    // update the posiion attributes
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  }
 }
---
+++ src/team-positions.ts
@@ -0,0 +1,66 @@
+export class TeamPositions {
+  public chars = [
+    {
+      id: 1,
+      name: "Sander",
+      image: "person.svg"
+    },
+    {
+      id: 2,
+      name: "Roy",
+      image: "person.svg"
+    },
+    {
+      id: 3,
+      name: "Roy",
+      image: "person.svg"
+    },
+    {
+      id: 4,
+      name: "Roy",
+      image: "person.svg"
+    }
+  ];
+  constructor() {
+    console.log('hier');
+    // this is used later in the resizing and gesture demos
+    (<any>window).dragMoveListener = this.dragMoveListener;
+    interact('.draggable')
+      .draggable({
+        // enable inertial throwing
+        inertia: true,
+        // keep the element within the area of it's parent
+        restrict: {
+          restriction: "parent",
+          endOnly: true,
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        },
+        // enable autoScroll
+        autoScroll: true,
+
+        // call this function on every dragmove event
+        onmove: this.dragMoveListener
+      });
+  }
+
+  public focusInput(charId: number) {
+    var doc = document.getElementById('char-' + charId);
+    doc.focus();
+  }
+
+  public dragMoveListener(event) {
+    var target = event.target,
+      // keep the dragged position in the data-x/data-y attributes
+      x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+      y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+      target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+}
